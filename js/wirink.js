4b63b78d612a12bfa9071fbade2431c665f6cac2
Elvis <jamoldinovbahodir195@gmail.com>
2023-05-28 23:51:25 +0500

    first commit

---
+++ js/wirink.js
@@ -0,0 +1,31 @@
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
+
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.opacity = 1;
+  } else {
+    backtop.style.opacity = 0;
+  }
+}
